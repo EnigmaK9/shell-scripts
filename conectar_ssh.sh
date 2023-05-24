04e4da5cbd8f0c932b7055bcd195319a6cddff58
Carlos Ignacio Padilla Herrera <enigmak9@protonmail.com>
2023-05-24 08:45:48 -0600

    changed password

--- conectar_ssh.sh
+++ conectar_ssh.sh
@@ -3,7 +3,7 @@
 # Variables de configuración
 direccion_remota=192.168.1.71
 usuario_remoto=enigma
-contrasena_remota=covid19
+contrasena_remota=gengar
 
 # Comando para conectarse por SSH
 sshpass -p "$contrasena_remota" ssh -o StrictHostKeyChecking=no $usuario_remoto@$direccion_remota
